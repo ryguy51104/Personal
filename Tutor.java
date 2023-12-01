ce66262862a85e26b5ba0c375f7c658287d8ba3a
ryguy51104 <ryan.cahill2@marist.edu>
2023-12-01 10:24:01 -0500

    create

---
+++ Tutor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Tutor
+ */
+public class Tutor {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("how old are you? ");
+        int num = input.nextInt();
+        System.out.println(num);
+
+        System.out.println("hello world");
+    }
+}
+
